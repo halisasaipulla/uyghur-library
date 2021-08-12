0f0315cab82d0cac0d499ba75a09c1b46ca14ee6
lisa1501 <halisasaipulla@gmail.com>
2021-08-12 12:30:54 -0700

    updated settings

--- uyghur_library/uyghur_library/settings.py
+++ uyghur_library/uyghur_library/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uyghur-library-capstone.herokuapp.com']
 
 
 # Application definition
